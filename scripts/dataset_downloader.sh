2a53bd87942a6b16ddd431952ae07c8706b02747
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-06-17 10:04:29 +0900

    modify

--- scripts/dataset_downloader.sh
+++ scripts/dataset_downloader.sh
@@ -1,38 +1,41 @@
 #!/usr/bin/env bash
-data_names=$@
-if [ $# -eq 0 ];
-then
-  echo "Slect data name from 'volleyball', 'collective-activity', 'ucf101'."
-  return 2
-fi
+mkdir data/
+
+# volleyball
+echo "Download volleyball"
+mkdir data/volleyball/
+cd data/volleyball/
+mkdir videos
+gdown 1DaUE3ODT_H5mBFi8JzOVBNzVldxfbPbX
+unzip volleyball.zip -d videos/
+cd ../../
 
 # collective activity
-if [ printf '%s\n' "$@" | grep -qx "collective-activity" ];
-then
-  echo "Download collective-activity"
-  mkdir data/collective-activity/
-  cd data/collective-activity/
-  wget http://cvgl.stanford.edu/data/ActivityDataset.zip
-  wget http://cvgl.stanford.edu/data/ActivityDataset2.tar.gz
-  unzip ActivityDataset.zip
-  tar -xzvf ActivityDataset2.tar.gz
-  rm ActivityDataset.zip
-  rm ActivityDataset2.tar.gz
-fi
+echo "Download collective-activity"
+mkdir data/collective-activity/
+cd data/collective-activity/
+wget http://cvgl.stanford.edu/data/ActivityDataset.zip
+wget http://cvgl.stanford.edu/data/ActivityDataset2.tar.gz
+unzip ActivityDataset.zip
+tar -xzvf ActivityDataset2.tar.gz
+rm ActivityDataset.zip
+rm ActivityDataset2.tar.gz
+cd ../../
 
 # ucf101
-if [ printf '%s\n' "$@" | grep -qx "ucf101" ];
-then
-  echo "Download ucf101"
-  mkdir data/ucf101
-  cd data/ucf101
-  mkdir videos
-  wget --no-check-certificate https://www.crcv.ucf.edu/data/UCF101/UCF101.rar
-  wget --no-check-certificate https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip
-  wget --no-check-certificate https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-DetectionTask.zip
-  unrar e UCF101.rar videos/
-  unzip UCF101TrainTestSplits-RecognitionTask.zip
-  unzip UCF101TrainTestSplits-DetectionTask.zip
-  mv ucfTrainTestlist/ recognition/
-  mv UCF101_Action_detection_splits/ detection/
-fi
+echo "Download ucf101"
+mkdir data/ucf101
+cd data/ucf101
+mkdir videos
+wget --no-check-certificate https://www.crcv.ucf.edu/data/UCF101/UCF101.rar
+wget --no-check-certificate https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip
+wget --no-check-certificate https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-DetectionTask.zip
+unrar e UCF101.rar videos/
+unzip UCF101TrainTestSplits-RecognitionTask.zip
+unzip UCF101TrainTestSplits-DetectionTask.zip
+mv ucfTrainTestlist/ recognition/
+mv UCF101_Action_detection_splits/ detection/
+rm UCF101.rar
+rm UCF101TrainTestSplits-RecognitionTask.zip
+rm UCF101TrainTestSplits-DetectionTask.zip
+cd ../../
