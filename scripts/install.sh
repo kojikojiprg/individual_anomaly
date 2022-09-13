a83df23aadea34bd85f78d98dcd3bf021d38a7ee
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-09-13 18:08:31 +0900

    update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,21 @@ pip install -r requirements.txt --extra-index-url https://download.pytorch.org/w
 
 git submodule update --init
 
+# installation for mmdet
+cd submodules/mmdet
+pip install -r requirements.txt
+pip install -v -e .
+cd ../../
+
 # installation for mmpose
 cd submodules/mmpose
 pip install -r requirements.txt
 pip install -v -e .
 cd ../../
+
+# install unitrack
+cd submodules/unitrack
+python setup.py
+# sklearn >= 0.23 changed this function name
+sed -i '/jaccard_similarity_score/d' utils/mask.py
+cd ../../  # go back root of the project
