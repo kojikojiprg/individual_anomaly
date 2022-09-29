704f452dd953d7242dc6cae13dbea841f6f03d31
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-09-29 16:16:12 +0900

    update requirements.txt setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 pip install -U pip
-pip install wheel
 pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu115
 
 git submodule update --init
 
-# installation for mmdet
-cd submodules/mmdet
-pip install -r requirements.txt
-pip install -v -e .
-cd ../../
+# install mmcv
+# cd submodules/mmcv
+# pip install -r requirements.txt
+# MMCV_WITH_OPS=1 pip install -e .
+# cd ../../
+# pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu115/torch1.12.1/index.html
 
-# installation for mmpose
-cd submodules/mmpose
-pip install -r requirements.txt
-pip install -v -e .
-cd ../../
+# install mmdet
+# cd submodules/mmdet
+# pip install -r requirements.txt
+# pip install -v -e .
+# cd ../../
+
+# install mmpose
+# cd submodules/mmpose
+# pip install -r requirements.txt
+# pip install -v -e .
+# cd ../../
 
 # install unitrack
 cd submodules/unitrack
+pip install imageio  # install one by one because of avoiding error
+pip install lap
+pip install cython
+pip install cython_bbox
 python setup.py
-# sklearn >= 0.23 changed this function name
-sed -i '/jaccard_similarity_score/d' utils/mask.py
+sed -i '/jaccard_similarity_score/d' utils/mask.py # sklearn >= 0.23 changed this function name
 cd ../../  # go back root of the project
