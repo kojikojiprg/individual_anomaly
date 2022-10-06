7afcc8bfb4880ebafd0828131c67244d74826d38
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-10-06 16:53:33 +0900

    update egan generator

--- modules/individual/model/egan/generator.py
+++ modules/individual/model/egan/generator.py
@@ -1,28 +1,25 @@
-import torch
 import torch.nn as nn
 
 from ..layers.embedding import Embedding
 from ..layers.positional_encoding import PositionalEncoding
-from ..layers.transformer import SpatialTemporalTransformer
+from ..layers.transformer import Encoder as TransformerEncoder
 
 
 class Generator(nn.Module):
     def __init__(self, config):
         super().__init__()
+        self.seq_len = config.seq_len
+        self.d_model = config.d_model
 
-        self.fc = nn.Linear(config.d_z, config.seq_len * 17 * 2)
+        self.emb = Embedding(config.d_z, config.seq_len * config.d_model)
 
-        self.emb_spat = Embedding(17 * 2, config.d_emb, config.d_model)
-        self.emb_temp = Embedding(config.seq_len, config.d_emb, config.d_model)
+        self.pe = PositionalEncoding(config.d_model, config.seq_len)
 
-        self.pe_spat = PositionalEncoding(config.d_model, config.seq_len)
-        self.pe_temp = PositionalEncoding(config.d_model, 17 * 2)
-
-        self.sttr = nn.ModuleList()
+        self.tr = nn.ModuleList()
         self.n_sttr = config.n_sttr
         for _ in range(config.n_sttr):
-            self.sttr.append(
-                SpatialTemporalTransformer(
+            self.tr.append(
+                TransformerEncoder(
                     config.d_model,
                     config.n_heads,
                     config.d_ff,
@@ -31,32 +28,23 @@ class Generator(nn.Module):
                 )
             )
 
-    def to(self, device):
-        self = super().to(device)
-        self.pe_spat.to(device)
-        self.pe_temp.to(device)
+        self.fc = nn.Linear(config.d_model, 17 * 2)
 
     def forward(self, z):
-        z = self.fc(z)
-
         B = z.shape[0]
-        z = z.view(B, -1, 34)
-        z_spat = z  # spatial
-        z_temp = z.permute(0, 2, 1)  # temporal
 
         # embedding
-        z_spat = self.emb_spat(z_spat)
-        z_temp = self.emb_temp(z_temp)
+        z = self.emb(z)
+        z = z.view(B, self.seq_len, self.d_model)  # B, T, D
 
         # positional encoding
-        z_spat = self.pe_spat(z_spat)
-        z_temp = self.pe_temp(z_temp)
+        z = self.pe(z)
 
         # spatial-temporal transformer
         for i in range(self.n_sttr):
-            z_spat, z_temp, weights_spat, weights_temp = self.sttr[i](z_spat, z_temp)
+            z, weights = self.tr[i](z)
 
-        z = torch.matmul(z_spat, z_temp.permute(0, 2, 1))
-        z = z.view((B, -1, 17, 2))
+        z = self.fc(z)
+        z = z.view(B, -1, 17, 2)  # B, T, 17, 2
 
-        return z, weights_spat, weights_temp
+        return z, weights
