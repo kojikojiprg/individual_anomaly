ba00ef650aa788c886a4177b060e09d661109212
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-10-06 16:54:51 +0900

    update embedding

--- modules/individual/model/layers/embedding.py
+++ modules/individual/model/layers/embedding.py
@@ -2,13 +2,11 @@ import torch.nn as nn
 
 
 class Embedding(nn.Module):
-    def __init__(self, d_input, d_emb, d_output):
+    def __init__(self, d_input, d_output):
         super().__init__()
 
-        self.linear = nn.Sequential(
-            nn.Linear(d_input, d_emb),
-            nn.Linear(d_emb, d_output),
-        )
+        self.linear = nn.Linear(d_input, d_output)
+        self.linear.requires_grad_ = False
 
     def forward(self, x):
         return self.linear(x)
