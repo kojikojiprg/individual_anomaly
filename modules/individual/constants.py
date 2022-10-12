9cc37f1eb74daa3f1d00a6f9bec248ecf1ae4477
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-10-12 15:47:43 +0900

    modify indivdidual constants

--- modules/individual/constants.py
+++ modules/individual/constants.py
@@ -6,15 +6,18 @@ from typing import List
 class IndividualDataFormat:
     frame_num: str = "frame"
     id: str = "id"
-    stage: str = "stage"
+    kps_real: str = "keypoints_real"
+    kps_fake: str = "keypoints_fake"
     z: str = "z"
     w_spat: str = "weights_spatial"
     w_temp: str = "weights_temporal"
+    f_real: str = "d_feature_real"
+    f_fake: str = "d_feature_fake"
     anomaly: str = "anomaly"
 
     @classmethod
-    def get_names(cls):
-        return list(cls.__dict__.keys())
+    def get_keys(cls) -> List[str]:
+        return [cls.__dict__[key] for key in cls.__annotations__.keys()]
 
 
 @dataclass(frozen=True)
@@ -25,8 +28,8 @@ class IndividualModelTypes:
 
     @classmethod
     def get_types(cls) -> List[str]:
-        return list(cls.__dict__.keys())
+        return [cls.__dict__[key] for key in cls.__annotations__.keys()]
 
     @classmethod
-    def includes(cls, model_type: str):
+    def includes(cls, model_type: str) -> bool:
         return model_type.casefold() in cls.get_types()
