f65b647383126f4802a02c4f0a9f3d840f594c77
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-10-24 11:15:17 +0900

    modify

--- modules/layers/embedding.py
+++ modules/layers/embedding.py
@@ -6,12 +6,15 @@ class Embedding(nn.Module):
         super().__init__()
 
         self.linear = nn.Linear(d_input, d_output)
-        self.linear.requires_grad_ = False
 
         # init weights
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
+
+        # stop updating params
+        for param in self.parameters():
+            param.requires_grad = False
 
     def forward(self, x):
         return self.linear(x)
