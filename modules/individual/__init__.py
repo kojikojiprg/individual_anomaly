4d1791121a5af6bfef6f0044dcf68bbc65c83122
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-10-26 15:20:55 +0900

    modify individual training log path

--- modules/individual/__init__.py
+++ modules/individual/__init__.py
@@ -82,7 +82,7 @@ class IndividualActivityRecognition:
 
     def _build_trainer(self, data_dir, gpu_ids):
         dataset_dir = os.path.dirname(data_dir)
-        log_path = os.path.join(dataset_dir, "logs", self._data_type)
+        log_path = os.path.join(dataset_dir, "logs", self._model_type, self._data_type)
 
         if hasattr(self._config.train, "accumulate_grad_batches"):
             accumulate_grad_batches = self._config.train.accumulate_grad_batches
