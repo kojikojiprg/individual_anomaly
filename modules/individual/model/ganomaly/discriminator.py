07d24b0f4b5d8b152a95737f6560b6cab984847a
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-10-29 22:05:37 +0900

    update individual ganomaly

--- modules/individual/model/ganomaly/discriminator.py
+++ modules/individual/model/ganomaly/discriminator.py
@@ -1,31 +1,22 @@
+import torch
 import torch.nn as nn
-from modules.individual import IndividualDataTypes
-from modules.layers.activation import Activation
 from modules.layers.embedding import Embedding
 from modules.layers.positional_encoding import PositionalEncoding
-from modules.layers.transformer import SpatialTemporalTransformer
+from modules.layers.transformer import Encoder as TransformerEncoder
 
 
 class Discriminator(nn.Module):
     def __init__(self, config, data_type):
         super().__init__()
+        self.emb = Embedding(17, config.d_model)
+        self.pe = PositionalEncoding(config.d_model, config.seq_len + 1)
+        self.cls = nn.Parameter(torch.randn((1, 1, config.d_model)))
 
-        if data_type == IndividualDataTypes.both:
-            self.n_kps = 34  # both
-        else:
-            self.n_kps = 17  # abs or rel
-
-        self.emb_in_spat = Embedding(self.n_kps * 2, config.d_model)
-        self.emb_in_temp = Embedding(config.seq_len, config.d_model)
-
-        self.pe_spat = PositionalEncoding(config.d_model, config.seq_len)
-        self.pe_temp = PositionalEncoding(config.d_model, self.n_kps * 2)
-
-        self.sttr = nn.ModuleList()
+        self.trs = nn.ModuleList()
         self.n_tr = config.n_tr
-        for _ in range(config.n_tr):
-            self.sttr.append(
-                SpatialTemporalTransformer(
+        for _ in range(self.n_tr):
+            self.trs.append(
+                TransformerEncoder(
                     config.d_model,
                     config.n_heads,
                     config.d_ff,
@@ -34,47 +25,22 @@ class Discriminator(nn.Module):
                 )
             )
 
-        self.emb_out_spat = Embedding(config.d_model, self.n_kps * 2)
-        self.emb_out_temp = Embedding(config.d_model, config.seq_len)
-        self.x_norm = nn.LayerNorm((config.seq_len, self.n_kps * 2))
-
-        self.ff = nn.Sequential(
-            nn.Linear(config.seq_len * self.n_kps * 2, config.d_out_feature),
-            Activation(config.activation),
-        )
+        self.ff = nn.Linear(config.d_model, config.d_out_feature)
+        self.selu = nn.SELU(inplace=True)
         self.out = nn.Linear(config.d_out_feature, 1)
 
-    def forward(self, x, mask=None):
-        B, T, P, D = x.shape  # batch, frame, num_points=17(or 34), dim=2
-        x = x.view(B, T, P * D)
-        x_spat = x  # spatial(B, T, 34(or 68))
-        x_temp = x.permute(0, 2, 1)  # temporal(B, 34(or 68), T)
+    def forward(self, x, mask):
+        B = x.size()[0]
+        x = self.emb(x)
 
-        if mask is not None:
-            mask = mask.view(B, T, P * D)
-            mask_spat = mask
-            mask_temp = mask.permute(0, 2, 1)
+        x = torch.cat([self.cls.repeat(B, 1, 1), x], dim=1)
+        x = self.pe(x)
 
-        # embedding
-        x_spat = self.emb_in_spat(x_spat)
-        x_temp = self.emb_in_temp(x_temp)
-
-        # positional encoding
-        x_spat = self.pe_spat(x_spat)
-        x_temp = self.pe_temp(x_temp)
-
-        # spatial-temporal transformer
         for i in range(self.n_tr):
-            x_spat, x_temp, _ = self.sttr[i](x_spat, x_temp, mask_spat, mask_temp)
-        x_spat = self.emb_out_spat(x_spat)
-        x_temp = self.emb_out_temp(x_temp)
-        x = x_spat + x_temp.permute(0, 2, 1)
-        x = self.x_norm(x)
+            x, _ = self.trs[i](x)
 
-        # concat x and z
-        feature = self.ff(x.view(B, -1))
-
-        # last layer
+        x = self.ff(x[:, 0])
+        feature = self.selu(x)
         pred = self.out(feature)
 
         return pred, feature
--- modules/individual/model/ganomaly/generator.py
+++ modules/individual/model/ganomaly/generator.py
@@ -1,130 +1,88 @@
+import copy
+
+import torch
 import torch.nn as nn
-from modules.individual import IndividualDataTypes
 from modules.layers.embedding import Embedding
 from modules.layers.positional_encoding import PositionalEncoding
+from modules.layers.transformer import Decoder as TransformerDecoder
 from modules.layers.transformer import Encoder as TransformerEncoder
-from modules.layers.transformer import SpatialTemporalTransformer
 
 
 class Generator(nn.Module):
     def __init__(self, config, data_type):
         super().__init__()
-        self.en = Encoder(config, data_type)
-        self.de = Decoder(config, data_type)
+        self.en = Encoder(config)
+        self.de = Decoder(config)
 
     def forward(self, x):
-        z, attn = self.en(x)
-        x = self.de(z)
-
-        return x, z, attn
+        z, attn_w = self.en(x)
+        x = self.de(x, z)
+        return x, z, attn_w
 
 
 class Encoder(nn.Module):
-    def __init__(self, config, data_type):
+    def __init__(self, config):
         super().__init__()
+        self.emb = Embedding(17, config.d_model)
+        self.pe = PositionalEncoding(config.d_model, config.seq_len + 1)
+        self.z = nn.Parameter(torch.randn((1, 1, config.d_model)))
 
-        if data_type == IndividualDataTypes.both:
-            self.n_kps = 34  # both
-        else:
-            self.n_kps = 17  # abs or rel
-
-        self.emb_spat = Embedding(self.n_kps * 2, config.d_model)
-        self.emb_temp = Embedding(config.seq_len, config.d_model)
-
-        self.pe_spat = PositionalEncoding(config.d_model, config.seq_len)
-        self.pe_temp = PositionalEncoding(config.d_model, self.n_kps * 2)
-
-        self.sttr = nn.ModuleList()
+        tre = TransformerEncoder(
+            config.d_model,
+            config.n_heads,
+            config.d_ff,
+            config.dropout,
+            config.activation,
+        )
         self.n_tr = config.n_tr_e
-        for _ in range(self.n_tr):
-            self.sttr.append(
-                SpatialTemporalTransformer(
-                    config.d_model,
-                    config.n_heads,
-                    config.d_ff,
-                    config.dropout,
-                    config.activation,
-                )
-            )
+        self.trs = nn.ModuleList([copy.deepcopy(tre) for _ in range(self.n_tr)])
 
-        self.fc_spat = Embedding(config.d_model, self.n_kps * 2)
-        self.fc_temp = Embedding(config.d_model, config.seq_len)
-
-        self.fc = nn.Linear(config.seq_len * self.n_kps * 2, config.d_z)
-        self.norm = nn.LayerNorm(config.d_z)
+        self.ff = nn.Linear(config.d_model, config.d_z)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        B, T, P, D = x.shape  # batch, frame, num_points=17, dim=2
-        x = x.view(B, T, P * D)
-        x_spat = x  # spatial(B, T, 34)
-        x_temp = x.permute(0, 2, 1)  # temporal(B, 34, T)
-
-        # embedding
-        x_spat = self.emb_spat(x_spat)
-        x_temp = self.emb_temp(x_temp)
-
-        # positional encoding
-        x_spat = self.pe_spat(x_spat)
-        x_temp = self.pe_temp(x_temp)
-
-        # spatial-temporal transformer
+        B = x.size()[0]
+        x = self.emb(x)
+        x = torch.cat([self.z.repeat(B, 1, 1), x], dim=1)
+        x = self.pe(x)
         for i in range(self.n_tr):
-            x_spat, x_temp, attn_weights = self.sttr[i](x_spat, x_temp)
-        x_spat = self.fc_spat(x_spat)
-        x_temp = self.fc_temp(x_temp)
+            x, attn_w = self.trs[i](x)
 
-        x = x_spat + x_temp.permute(0, 2, 1)
-        x = self.fc(x.view(B, -1))
-        z = self.norm(x)
-
-        return z, attn_weights
+        z = self.sigmoid(self.ff(x[:, 0]))
+        return z, attn_w
 
 
 class Decoder(nn.Module):
-    def __init__(self, config, data_type):
+    def __init__(self, config):
         super().__init__()
-
-        if data_type == IndividualDataTypes.both:
-            self.n_kps = 34  # both
-        else:
-            self.n_kps = 17  # abs or rel
-        self.seq_len = config.seq_len
-        self.d_model = config.d_model
-
-        self.emb = Embedding(config.d_z, config.seq_len * config.d_model)
-
+        self.emb = Embedding(17, config.d_model)
         self.pe = PositionalEncoding(config.d_model, config.seq_len)
 
-        self.tr = nn.ModuleList()
+        self.linear_z = nn.Linear(config.d_z, config.d_model)
+
+        trd = TransformerDecoder(
+            config.d_model,
+            config.n_heads,
+            config.d_ff,
+            config.dropout,
+            config.activation,
+        )
         self.n_tr = config.n_tr_d
-        for _ in range(self.n_tr):
-            self.tr.append(
-                TransformerEncoder(
-                    config.d_model,
-                    config.n_heads,
-                    config.d_ff,
-                    config.dropout,
-                    config.activation,
-                )
-            )
+        self.trs = nn.ModuleList([copy.deepcopy(trd) for _ in range(self.n_tr)])
 
-        self.fc = nn.Linear(config.d_model, self.n_kps * 2)
+        self.ff = nn.Linear(config.d_model, 17 * 2)
+        self.sigmoid = nn.Sigmoid()
 
-    def forward(self, z):
-        B = z.shape[0]
+    def forward(self, x, z):
+        B = x.size()[0]
+        x = self.emb(x)
+        x = self.pe(x)
 
-        # embedding
-        z = self.emb(z)
-        z = z.view(B, self.seq_len, self.d_model)  # B, T, D
+        z = self.linear_z(z)
 
-        # positional encoding
-        z = self.pe(z)
-
-        # spatial-temporal transformer
         for i in range(self.n_tr):
-            z, _ = self.tr[i](z)
+            x, attn_w = self.trs[i](x, z.unsqueeze(1))
 
-        z = self.fc(z)
-        x = z.view(B, -1, self.n_kps, 2)  # B, T, 17, 2
-
+        x = self.sigmoid(self.ff(x))
+        x = x.view(B, -1, 17, 2)
         return x
