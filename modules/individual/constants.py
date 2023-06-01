048f2f8f8c8eb699035a3f45578d42afec5e271a
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2023-06-01 10:40:16 +0900

    modify individual data format

--- modules/individual/constants.py
+++ modules/individual/constants.py
@@ -16,15 +16,22 @@ class _AbstractDataClass:
 class IndividualDataFormat(_AbstractDataClass):
     frame_num: str = "frame"
     id: str = "id"
-    roll_label: str = "roll_label"
+
     kps_real: str = "keypoints_real"
     kps_fake: str = "keypoints_fake"
+
     z: str = "z"
     attn: str = "attn_weights"
+
     f_real: str = "d_feature_real"
     f_fake: str = "d_feature_fake"
+
     loss_r: str = "loss_resitudal"
     loss_d: str = "loss_discrimination"
+
+    roll_label: str = "roll_label"
+    role_adv: str = "role_adv"
+    role_aux: str = "role_aux"
 
 
 @dataclass(frozen=True)
