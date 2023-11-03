e1cb10b24acb4b5a92fb4b0513dbbd8658a5e1cd
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2023-11-03 12:22:35 +0900

    delete pose estimation

--- modules/pose/format.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class Format:
-    frame_num: str = "frame"
-    id: str = "id"
-    bbox: str = "bbox"
-    keypoints: str = "keypoints"
